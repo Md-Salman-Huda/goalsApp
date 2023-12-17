4d6746a711a086f7a6c80fc6760719d2210946f3
Md Salman Huda <n01551241@humbermail.ca>
2023-12-17 10:04:44 -0500

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT =process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message:"get goals"});
+})
+
+app.listen(PORT, ()=>console.log(`App listening to ${PORT}`));
